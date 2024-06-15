30be2666bf36b0babedbcc9f8e0ee9ba2ffaf56e
Scott Boms <scottboms@gmail.com>
2024-06-14 18:09:31 -0700

    Updating plugin info to utilize the new syntax for Kirby 4.3. Version bumped to 1.1.1.

--- index.php
+++ index.php
@@ -5,34 +5,39 @@
 /**
  * Kirby kbd KirbyTag
  *
- * @version 1.0.0
  * @author Scott Boms <plugins@scottboms.com>
- * @copyright Scott Boms <plugins@scottboms.com>
  * @link https://github.com/scottboms/kirbytag-kbd
  * @license MIT
 **/
 
-Kirby::plugin('scottboms/kirbytag-kbd', [
-  'tags' => [
-    'kbd' => [
-      'attr' => [
-        'class',
-      ],
-      'html' => function($tag) {
-        $html = '';
-        // Variables to hold tag options
-        $kbd_string = $tag->kbd;
-        $class = $tag->class;
+Kirby::plugin(
+  name: 'scottboms/kirbytag-kbd', 
+  info: [
+    'homepage' => 'https://github.com/scottboms/kirbytag-kbd'
+  ],
+  version: '1.0.1',
+  extends: [
+    'tags' => [
+      'kbd' => [
+        'attr' => [
+          'class',
+        ],
+        'html' => function($tag) {
+          $html = '';
+          // Variables to hold tag options
+          $kbd_string = $tag->kbd;
+          $class = $tag->class;
 
-        if($class == '') {
-          // If no class attribute, output the <kbd></kbd>
-          $html .= '<kbd>' . $kbd_string . '</kbd>';
-        } else {
-          $html .= '<kbd class="' . $class . '">' . $kbd_string . '</kbd>';
+          if($class == '') {
+            // If no class attribute, output the <kbd></kbd>
+            $html .= '<kbd>' . $kbd_string . '</kbd>';
+          } else {
+            $html .= '<kbd class="' . $class . '">' . $kbd_string . '</kbd>';
+          }
+
+          return $html;
         }
-
-        return $html;
-      }
+      ]
     ]
   ]
-]);
+);
